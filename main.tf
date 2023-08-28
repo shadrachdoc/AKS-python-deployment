e86c4f497036756c3d081e8604edbcfce85f85c5
shadrachdoc <41693892+shadrachdoc@users.noreply.github.com>
2023-08-28 11:17:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ terraform {
     storage_account_name = "tfstatestorageacct34"
     container_name       = "tfstatefiles"
     key                  = "prod/terraform/tfstate"
+    client_id             = "8826a619-e971-421c-8230-cac671d17289"
+    client_secret         = "vqa8Q~uHiv8lpHtM3RCDXjACaeRHv7aml_7Z8btV"
+    tenant_id             = "5a8844e2-dc20-4f80-b99e-77196d375a40"
   }
 }
 
