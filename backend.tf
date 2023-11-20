85e7ebca1a7f2d0c3be41fe69bb8ce4e36234fc7
shadrachdoc <41693892+shadrachdoc@users.noreply.github.com>
2023-11-20 12:58:56 +0000

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "test_env"
-    storage_account_name = "statefile2245"
-    container_name       = "statefiles"
-    key                  = "terraform.tfstate"
-  }
-}
