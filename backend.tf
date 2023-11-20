a35dc0301d1058d9b9be5757a86c1ea0f18e5bdb
shadrachdoc <41693892+shadrachdoc@users.noreply.github.com>
2023-11-20 12:48:51 +0000

    Update and rename backend.yaml to backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "test"
+    resource_group_name  = "test_env"
     storage_account_name = "statefile2245"
     container_name       = "statefiles"
     key                  = "terraform.tfstate"
