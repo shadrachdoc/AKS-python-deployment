cf12733c9b780c5766502b9b78ed406ca69c274f
shadrach <schelladurai@cynergybank.co.uk>
2023-11-21 07:33:11 +0000

    health path added in py app

--- py_flash/app.py
+++ py_flash/app.py
@@ -5,9 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello I am  Docker image inside from Azure AKS"
+    return "Hello, I am a Docker image running inside Azure AKS!"
 
+@app.route("/healthz")
+def health():
+    # Add health check logic here
+    # For example, check database connections, external service availability, etc.
+    return "I am healthy!", 200
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
+
